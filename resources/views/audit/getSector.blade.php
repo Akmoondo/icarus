61fc94f5be83d53182083341c8af5c7cc9b1b030
Akmoondo <nicolas.lsortiz@gmail.com>
2019-11-10 17:16:55 -0300

    canseideinventarnome

--- resources/views/audit/getSector.blade.php
+++ resources/views/audit/getSector.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 @foreach ($sectors as $sector)
-<div class="col-md-12 col-sm-12 col-xs-30">
-    <div class="x_panel tile fixed_height_320 overflow_hidden">
+<div class="col-md-12 col-sm-8 col-xs-30">
+    <div class="x_panel tile fixed_height_100 overflow_hidden">
     <div class="x_title">
     <h2>{{$sector->sector}}</h2>
     <ul class="nav navbar-right panel_toolbox">
@@ -15,44 +15,25 @@
     </ul>
     <div class="clearfix"></div>
     </div>
-    <div class="x_content">
-    <!--<table class="" style="width:100%">
-    <tbody><tr>
-    <th style="width:37%;">
-    <p>Gráfico</p>
-    </th>
-    <th>
-    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-7">
-    <p class="">Situação</p>
+    <div class="x_content" style="display: block;">
+            <div class="widget_summary">
+            <div class="w_left w_25">
+            <span>0.1.5.2</span>
+            </div>
+            <div class="w_center w_55">
+            <div class="progress">
+            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 4%;">
+            <span class="sr-only">60% Complete</span>
+            </div>
+            </div>
+            </div>
+            <div class="clearfix"></div>
+            </div>
     </div>
-    <div class="pull-right">
-    <p class="">Conformidade</p>
-    </div>
-    </th>
-    </tr>
-    <tr>
-    <td><iframe class="chartjs-hidden-iframe" style="width: 100%; display: block; border: 0px; height: 0px; margin: 0px; position: absolute; left: 0px; right: 0px; top: 0px; bottom: 0px;"></iframe>
-    <canvas class="canvasDoughnut" height="140" width="140" style="margin: 15px 10px 10px 0px; width: 140px; height: 140px;"></canvas>
-    </td>
-    <td>
-    <table class="tile_info">
-    <tbody>
-    {{--@foreach ($situations as $situation)
-        <tr>
-            <td>
-            <p><i class="fa fa-square {{$situation->color}}"></i>{{$situation->situation}}</p>
-            </td>
-            <td>Contar</td>
-        </tr> 
-    @endforeach
-    --}}
-    </tbody></table>
-    </td>
-    </tr>
-    </tbody></table>-->
+
     </div>
     </div>
-    </div>
+    
     <br/>
 
     @endforeach
--- resources/views/layouts/barra-topo.blade.php
+++ resources/views/layouts/barra-topo.blade.php
@@ -6,18 +6,10 @@
     <ul class="nav navbar-nav navbar-right">
       <li class="">
         <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-          <img src="{{ asset('/gentelella')}}/images/img.jpg" alt="">John Doe
+          John Doe
           <span class=" fa fa-angle-down"></span>
         </a>
         <ul class="dropdown-menu dropdown-usermenu pull-right">
-          <li><a href="javascript:;"> Profile</a></li>
-          <li>
-            <a href="javascript:;">
-              <span class="badge bg-red pull-right">50%</span>
-              <span>Settings</span>
-            </a>
-          </li>
-          <li><a href="javascript:;">Help</a></li>
           <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
         </ul>
       </li>
