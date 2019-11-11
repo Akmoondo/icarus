da86d4206cb67f64801f426bb9c139b5bf081462
Akmoondo <nicolas.lsortiz@gmail.com>
2019-11-10 22:09:20 -0300

    falta4dias

--- resources/views/layouts/barra-topo.blade.php
+++ resources/views/layouts/barra-topo.blade.php
@@ -6,7 +6,7 @@
     <ul class="nav navbar-nav navbar-right">
       <li class="">
         <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-          John Doe
+          Administrador
           <span class=" fa fa-angle-down"></span>
         </a>
         <ul class="dropdown-menu dropdown-usermenu pull-right">
@@ -14,70 +14,5 @@
         </ul>
       </li>
       
-      <!--
-      <li role="presentation" class="dropdown">
-        <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-          <i class="fa fa-envelope-o"></i>
-          <span class="badge bg-green">6</span>
-        </a>
-        <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-          <li>
-            <a>
-              <span class="image"><img src="{{ asset('/gentelella')}}/images/img.jpg" alt="Profile Image"></span>
-              <span>
-                <span>John Smith</span>
-                <span class="time">3 mins ago</span>
-              </span>
-              <span class="message">
-                Film festivals used to be do-or-die moments for movie makers. They were where...
-              </span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="image"><img src="{{ asset('/gentelella')}}/images/img.jpg" alt="Profile Image"></span>
-              <span>
-                <span>John Smith</span>
-                <span class="time">3 mins ago</span>
-              </span>
-              <span class="message">
-                Film festivals used to be do-or-die moments for movie makers. They were where...
-              </span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="image"><img src="{{ asset('/gentelella')}}/images/img.jpg" alt="Profile Image"></span>
-              <span>
-                <span>John Smith</span>
-                <span class="time">3 mins ago</span>
-              </span>
-              <span class="message">
-                Film festivals used to be do-or-die moments for movie makers. They were where...
-              </span>
-            </a>
-          </li>
-          <li>
-            <a>
-              <span class="image"><img src="{{ asset('/gentelella')}}/images/img.jpg" alt="Profile Image"></span>
-              <span>
-                <span>John Smith</span>
-                <span class="time">3 mins ago</span>
-              </span>
-              <span class="message">
-                Film festivals used to be do-or-die moments for movie makers. They were where...
-              </span>
-            </a>
-          </li>
-          <li>
-            <div class="text-center">
-              <a>
-                <strong>See All Alerts</strong>
-                <i class="fa fa-angle-right"></i>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </li>-->
     </ul>
   </nav>
